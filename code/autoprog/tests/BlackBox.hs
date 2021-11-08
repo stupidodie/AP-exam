4154f3171146bc6828f8a41cbb76e35a123d291c
GuanRan Tai <taiguanran@gmail.com>
2021-11-08 09:33:34 +0100

    add some test cases

--- code/autoprog/tests/BlackBox.hs
+++ code/autoprog/tests/BlackBox.hs
@@ -12,6 +12,7 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Resolver (declare)
 
+import Debug.Trace
 main :: IO ()
 main = defaultMain $ localOption (mkTimeout 1000000) tests
 
@@ -20,7 +21,25 @@ tests = testGroup "Minimal tests" [
     testCase "...Type" $
       parseStringType "a -> a" @?= Right pt0,
     testCase "...TDeclz" $
-      parseStringTDeclz "type T a = a -> a" @?= Right [td0]
+      parseStringTDeclz "type T a = a -> a" @?= Right [td0],
+    testCase "type2" $
+      parseStringType "F G x" @?= Right pt2,
+    testCase "type3" $
+      parseStringType "F G  a->B x" @?= Right (PTApp "(->)" [PTApp "F" [PTApp "G" [],PTVar "a"],PTApp "B" [PTVar "x"]]),
+    testCase "type4" $
+      parseStringType "F x -> (y, A)" @?= Right (PTApp "(->)" [PTApp "F" [PTVar "x"],PTApp "(,)" [PTVar "y",PTApp "A" []]]),
+    testCase "type5 with comment" $
+      parseStringType "z\'123 -> z{-12 -} -> zzz" @?= Right (PTApp "(->)" [PTVar "z'123",PTApp "(->)" [PTVar "z",PTVar "zzz"]]),
+    testCase "type6 with comment" $
+      parseStringType "z\'123 -> z{-12 -} z3-}" @?= Left "parse Error",
+    testCase "new type 1" $
+      parseStringTDeclz "newtype Reader r a = Rd {runRd :: r -> a}" @?= Right [TDRcd ("Reader",["r","a"]) "Rd" [("runRd",PTApp "(->)" [PTVar "r",PTVar "a"])]],
+    testCase "new type 2" $
+      parseStringTDeclz "newtype State s a = St {runSt :: s -> (a,s)}" @?= Right [TDRcd ("State",["s","a"]) "St" [("runSt",PTApp "(->)" [PTVar "s",PTApp "(,)" [PTVar "a",PTVar "s"]])]],
+    testCase "type 1" $
+      parseStringTDeclz "type F x = x -> x" @?= Right [TDSyn ("F",["x"]) (PTApp "(->)" [PTVar "x",PTVar "x"])],
+    testCase "data "$ 
+      parseStringTDeclz "data State s a = St {runSt,b,c :: s -> (a,s)}" @?= Right [TDRcd ("State",["s","a"]) "St" [("runSt",PTApp "(->)" [PTVar "s",PTApp "(,)" [PTVar "a",PTVar "s"]]),("b",PTApp "(->)" [PTVar "s",PTApp "(,)" [PTVar "a",PTVar "s"]]),("c",PTApp "(->)" [PTVar "s",PTApp "(,)" [PTVar "a",PTVar "s"]])]]
   ],
   testGroup "Resolver" [
     testCase "resolve" $
@@ -32,9 +51,19 @@ tests = testGroup "Minimal tests" [
       @?= Right st0,
     testCase "declare2" $
       do tce<- declare [td1] 
-         tf1 <- case lookup "C" tce of Just tf -> return tf; _ -> Left "no C" 
+         tf1 <- case lookup "T" tce of Just tf -> return tf; _ -> Left "no T" 
          tf1 [STVar "bbb"]
-      @?= Right st1
+      @?= Right st1,
+    testCase "declare3" $
+      do tce<- declare [td3] 
+         tf1 <- case lookup "State" tce of Just tf -> return tf; _ -> Left "no T" 
+         tf1 [STVar "bbb",STVar "ccc"]
+      @?= Right st3,
+    testCase "declare4" $
+      do tce<- declare [td4] 
+         tf1 <- case lookup "State" tce of Just tf -> return tf; _ -> Left "no T" 
+         tf1 [STVar "bbb",STVar "ccc"]
+      @?= Right st4
   ],
   testGroup "Coder" [
     testCase "pick" $
@@ -45,6 +74,8 @@ tests = testGroup "Minimal tests" [
       @?= tr0,
     testCase "solutions" $
       solutions tr0 10 Nothing @?= [3,4],
+    testCase "solutions 2" $
+      solutions tr0 3 (Just 5) @?= [3,5],
     testCase "produce" $
       do e <- dfs (produce [] st0)
          return $ case e of
@@ -54,10 +85,17 @@ tests = testGroup "Minimal tests" [
     ]]
  where pt0 = PTApp "(->)" [PTVar "a", PTVar "a"]
        td0 = TDSyn ("T", ["a"]) pt0
+       td3= TDRcd ("State",["s","a"]) "St"  pt3
+       pt3= [("runSt",PTApp "(->)" [PTVar "s",PTApp "(,)" [PTVar "a",PTVar "s"]])]
+       st3= STRcd "St" [("runSt",STArrow (STVar "bbb") (STProd (STVar "ccc") (STVar "bbb")))]
        st0 = STArrow (STVar "a'") (STVar "a'")
        tr0 = Choice [Choice [Found 4, Choice []], Found 3]
+       td4=TDRcd ("State",["s","a"]) "St" pt4
+       pt4=[("b",PTApp "(->)" [PTVar "s",PTApp "(->)" [PTVar "a",PTVar "s"]])]
+       st4= STRcd "St" [("b",STArrow (STVar "bbb") (STArrow (STVar "ccc") (STVar "bbb")))]
        dfs (Found a) = [a]
        dfs (Choice ts) = concatMap dfs ts
        e0 = Lam "X" (Var "X")
        td1=TDRcd ("T", ["a"]) "C" [("x", PTVar "a"), ("f", PTApp "(->)" [PTVar "a", PTVar "a"])]
        st1=STRcd "C" [("x",STVar "bbb"),("f",STArrow (STVar "bbb") (STVar "bbb"))]
+       pt2=PTApp "F" [PTApp "G" [],PTVar "x"]
